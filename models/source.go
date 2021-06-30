c201a494c7a9ff70062147aa1efc10f7c3aaa0db
gaelL <contact@gaell.info>
2021-06-30 15:22:49 +0200

    moved to cycloidio/cycloid-resource

--- models/source.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Source struct {
-	ApiKey   string `json:"api_key"`
-	Org      string `json:"org"`
-	Project  string `json:"project"`
-	Env      string `json:"env"`
-	ApiURL   string `json:"api_url"`
-}
--- models/version.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Version struct {
-	BuildID    string `json:"build_id"`
-	Criticals  string `json:"criticals"`
-	Warnings   string `json:"warnings"`
-	Advisories string `json:"advisories"`
-}
